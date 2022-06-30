3e1dab2a848c4ad6c8dd6894fc6fe80c4159f16d
lenders-game <98999083+lenders-game@users.noreply.github.com>
2022-06-30 13:25:21 -0700

    delete functionality added

--- ui/src/pages/PostDetails.jsx
+++ ui/src/pages/PostDetails.jsx
@@ -1,11 +1,33 @@
 import React, { useContext } from 'react';
 import { BlogContext } from '../App.js'
 import '../styles/PostDetails.css'
+import { useNavigate } from 'react-router-dom';
+
 
 const PostDetails = () => {
   const bc = useContext(BlogContext)
-
+  const nav = useNavigate()
   console.log(bc.selected)
+
+  const deleteHandler = () => {
+    const request = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(bc.selected)
+    } 
+    fetch(bc.serverUrl+'/api/posts/', request)
+      .then(res => res.json())
+      .then(data => {
+        bc.refresh()
+        nav('/myposts')
+        alert('Post deleted')
+      })
+      .catch(err => console.log(err))
+  }
+
+  
 
   
   return (
@@ -18,7 +40,16 @@ const PostDetails = () => {
       <div className='content-wrapper'>
         {bc.selected.content}
       </div>
-      
+      {
+        bc.user.id !== undefined ?
+        <span className='button-wrapper'>
+          <button className='delete-button' onClick={deleteHandler}>Delete</button>
+          <button className='edit-button' onClick={editHandler}>Edit</button>
+        </span>
+        :
+        ''
+      }
+         
     </div>
   )
 
